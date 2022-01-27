aa124f3f4913187d9748930e310d92e172e73532
AndreyKa <andreykashk@gmail.com>
2022-01-27 08:38:55 +0200

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketOpen = [];
+  const bracketClose = [];
+  const res = [];
+
+  bracketsConfig.forEach(function(arr) {
+      bracketOpen.push(arr[0]);
+      bracketClose.push(arr[1])
+  });
+
+  [...str].forEach(function (item) {
+    if (bracketOpen.indexOf(item) >= 0 && (bracketClose.indexOf(item) < 0 || res.indexOf(item) < 0)) {
+      res.push(item)
+      return
+    } 
+    if (bracketClose.indexOf(item) >= 0 && bracketOpen[bracketClose.indexOf(item)] === res[res.length - 1]) {
+      res.pop()
+      return
+    } else {
+      res.push(item);
+    }
+  })
+
+  if (res.length === 0 ) {
+    return true
+  } else {
+    return false
+  }
 }
+
+// console.log(check('([[[[]]]]))', [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]))
